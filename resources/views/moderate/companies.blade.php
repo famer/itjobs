0ad731c9d2d6335f3f9f3e50e43264a22d335886
Timur Tatarshaov <dj.famer@gmail.com>
2023-04-19 20:15:11 +0300

    bold font for companies in moderation

--- resources/views/moderate/companies.blade.php
+++ resources/views/moderate/companies.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     @forelse ($companies as $company)
-        {{ $company->name }}<br />
+        <h3 class="font-bold">{{ $company->name }}</h3>
 
         <a class="hover:bg-red-200 bg-red-300 px-2 py-1.5 rounded text-red-500" href="{{ route('moderate.company.form', $company) }}">Nope, edit</a>
         <form class="inline" method="post" action="{{ route('moderate.company', $company) }}">
