6eeff93e29b17f938bf8d39c26e3e3421edd2c7e
Timur Tatarshaov <dj.famer@gmail.com>
2023-04-21 05:12:54 +0300

    verification email customization

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvid
 use App\Models\User;
 use App\Models\Position;
 use App\Models\Company;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -50,6 +52,13 @@ class AuthServiceProvider extends ServiceProvider
 
         Gate::define('update-position', function (User $user, Position $position) {
             return $user->isAdmin() || ( $user->type == 'employer' && $position->company->user->id === $user->id);
+        });
+
+        VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->line('Click the button below to verify your email address.')
+                ->action('Verify Email Address!', $url);
         });
     }
 }
