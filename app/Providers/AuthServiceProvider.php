ff585561687b790accdc10d67a735e5415bd945e
Timur Tatarshaov <dj.famer@gmail.com>
2023-05-08 22:27:18 +0300

    email confirmation tidy up

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -56,9 +56,11 @@ class AuthServiceProvider extends ServiceProvider
 
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
             return (new MailMessage)
-                ->subject('Verify Email Address')
-                ->line('Click the button below to verify your email address.')
-                ->action('Verify Email Address!', $url);
+                ->greeting(__('email.Greeting'))
+                ->subject(__('email.Subject'))
+                ->line(__('email.Line'))
+                ->salutation(__('email.Salutation'))
+                ->action(__('email.Action'), $url);
         });
     }
 }
