abf3fd7055fe766fc8c38931e3a064a531809505
Timur Tatarshaov <dj.famer@gmail.com>
2023-05-09 16:12:20 +0300

    remove login-otp from the repo

--- docker/adminer/login-otp.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('plugins/login-otp.php');
-
-/** 
-* @param string decoded secret, e.g. base32_decode("SECRET")
-*/
-return new AdminerLoginOtp(
-	base64_decode('sj/OjfaO4Ew+VQ==')
-);
