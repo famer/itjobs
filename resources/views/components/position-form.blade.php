231bd75a28f64bbf08a11513fe72612bb195730c
Timur Tatarshaov <dj.famer@gmail.com>
2023-05-09 19:02:59 +0300

    position form tidy up

--- resources/views/components/position-form.blade.php
+++ resources/views/components/position-form.blade.php
@@ -1,11 +1,11 @@
 @props(['company' => $company])
 
-<div class="m-4">
+<div class="m-4 max-w-md">
     <form action="{{ route('position.post') }}" method="post">
         @csrf
         <input type="hidden" name="company_id" value="{{ $company->id }}">
         <div>
-            <input required class="w-1/4 px-2 py-1.5 border border-gray-300 border-2 rounded-lg" type="text" name="title" placeholder="{{ __('positions.Title') }}" value="{{ old('title') }}">
+            <input required class="relative block w-full px-2 py-1.5 border border-gray-300 border-2 rounded-lg" type="text" name="title" placeholder="{{ __('positions.Title') }}" value="{{ old('title') }}">
             @error('title')
                 <div class="text-red-500 my-2 text-sm">
                     {{ $message }}
@@ -13,13 +13,13 @@
             @enderror
         </div>
         <div>
-            <textarea required placeholder="{{ __('positions.Description') }}" class="w-1/4 px-2 py-1.5 border border-gray-300 border-2 rounded-lg" name="description">{{ old('description') }}</textarea> 
+            <textarea required placeholder="{{ __('positions.Description') }}" class="relative block w-full px-2 py-1.5 border border-gray-300 border-2 rounded-lg" name="description">{{ old('description') }}</textarea> 
             @error('description')
                 <div class="text-red-500 my-2 text-sm">
                     {{ $message }}
                 </div>
             @enderror
         </div>
-        <button class="btn btn-blue" type="submit">{{ __('positions.Add') }}</button>
+        <button class="mt-2 btn btn-blue" type="submit">{{ __('positions.Add') }}</button>
     </form>
 </div>
