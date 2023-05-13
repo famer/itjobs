6ff6a5055def0ec38549af41fbce72985ba40e97
Timur Tatarshaov <dj.famer@gmail.com>
2023-05-13 11:35:01 +0300

    remove .swp file from gitignore

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -7,6 +7,7 @@
         @vite('resources/css/app.css')
         <script src="https://code.jquery.com/jquery-3.6.4.slim.min.js" integrity="sha256-a2yjHM4jnF9f54xUQakjZGaqYs/V1CYvWpoqZzC2/Bw=" crossorigin="anonymous"></script>
         <script src="/js/jquery.cookie.js"></script>
+        @include('layout.ymetrika')
         <!--script src="https://cdn.tailwindcss.com"></script-->
     </head>
     <body>
